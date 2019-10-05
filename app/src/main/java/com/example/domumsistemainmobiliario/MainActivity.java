504398eea38d393a8967502f59dce660d327e78c
Matias Torres <matitorres87@gmail.com>
2019-10-04 22:15:26 -0300

    Fragment map Dialog perfil

--- app/src/main/java/com/example/domumsistemainmobiliario/MainActivity.java
+++ app/src/main/java/com/example/domumsistemainmobiliario/MainActivity.java
@@ -4,22 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppCompatActivity {
 
-    private GoogleMap map;
+    private AlertDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +32,7 @@ public class MainActivity extends FragmentActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle(" Propiedades");
         toolbar.setLogo(R.mipmap.logo_domum_round);
-        ((AppCompatActivity)getApplicationContext()).setSupportActionBar(toolbar);
+        setSupportActionBar(toolbar);
 
         // Configura la función del botón flotante
         FloatingActionButton fab = findViewById(R.id.fab_notificaciones);
@@ -53,8 +55,6 @@ public class MainActivity extends FragmentActivity {
             // Mostrar propiedades
             MapFragment mapFragment = new MapFragment();
             getSupportFragmentManager().beginTransaction().add(R.id.container, mapFragment).commit();
-            ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_map))
-                    .getMapAsync(new MapaPropiedades());
         }
     }
 
@@ -68,7 +68,29 @@ public class MainActivity extends FragmentActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_perfil:
-                // User chose the "Settings" item, show the app settings UI...
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                View view = getLayoutInflater().inflate(R.layout.dialog_perfil, null);
+
+                Button btnGuardar = view.findViewById(R.id.btnGuardar);
+                btnGuardar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(MainActivity.this, "Actualización exitosa", Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                    }
+                });
+
+                Button btnCancelar = view.findViewById(R.id.btnCancelar);
+                btnCancelar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+
+                builder.setView(view);
+                dialog = builder.create();
+                dialog.show();
                 return true;
 
             case R.id.action_salir:
@@ -81,13 +103,4 @@ public class MainActivity extends FragmentActivity {
 
         }
     }
-
-    private class MapaPropiedades implements OnMapReadyCallback {
-
-        @Override
-        public void onMapReady(GoogleMap googleMap) {
-            map = googleMap;
-        }
-    }
-
 }
--- app/src/main/java/com/example/domumsistemainmobiliario/MapFragment.java
+++ app/src/main/java/com/example/domumsistemainmobiliario/MapFragment.java
@@ -1,17 +1,75 @@
 package com.example.domumsistemainmobiliario;
 
+import android.content.res.Resources;
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class MapFragment extends Fragment {
+import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapFragment extends SupportMapFragment implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener  {
+
+    static final LatLng PROPIEDAD_UNO = new LatLng(-32.410307, -65.675051);
+    static final LatLng PROPIEDAD_DOS = new LatLng(-32.541867, -65.645239);
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+
+        getMapAsync(this);
+
+        return rootView;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            getContext(), R.raw.mapstyle));
+
+            if (!success) {
+                Log.e("MapFragment", "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e("MapFragment", "Can't find style. Error: ", e);
+        }
+
+        googleMap.getUiSettings().setMapToolbarEnabled(false);
+
+        googleMap.setOnMarkerClickListener((GoogleMap.OnMarkerClickListener) this);
+
+        float zoom = 10;
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(PROPIEDAD_UNO, zoom));
+
+        googleMap.addMarker(new MarkerOptions().position(PROPIEDAD_UNO));
+        googleMap.addMarker(new MarkerOptions().position(PROPIEDAD_DOS));
+
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        InmuebleFragment inmuebleFragment = new InmuebleFragment();
+
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, inmuebleFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+
+        return true;
     }
 }
